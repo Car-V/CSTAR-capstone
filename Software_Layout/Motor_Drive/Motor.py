82cc56d298639cd8210ebf15dfd337ec736ef2d3
Car-V <vale.c@northeastern.edu>
2025-01-28 19:10:16 -0500

    Motor accel/decel, LiDAR beginnings

--- Software_Layout/Motor_Drive/Motor.py
+++ Software_Layout/Motor_Drive/Motor.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as GPIO          
 from time import sleep
 from Encoder import Encoder
+
+MAX_DUTY_CYCLE = 25
+
 class Motor:
     def __init__(self, pin1_num: int, pin2_num: int, en_num: int, pin_a: int, pin_b: int):
         self.pin1 = pin1_num
         self.pin2 = pin2_num
         self.enable = en_num
         self.PWMhz = 1000
-        self.duty_cycle = 25
+        self.duty_cycle = MAX_DUTY_CYCLE
         self.encoder = Encoder(pin_a, pin_b)
-        
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin1, GPIO.OUT)
@@ -38,6 +40,16 @@ class Motor:
         self.duty_cycle = duty_cycle
         self.p.ChangeDutyCycle(self.duty_cycle)
 
+    # ensure the increments we decelerate or accelerate by are a whole factor of max duty cycle
+    def accelerate(self):
+        set_speed(duty_cycle + 5)
+        sleep(0.25)
+        print("Duty cycle is " + duty_cycle)
+
+    def decelerate(self):
+        set_speed(duty_cycle - 5)
+        sleep(0.25)
+        print("Duty cycle is " + duty_cycle)
 
     def get_encoder_position(self):
         return self.encoder.get_position()
--- Software_Layout/Motor_Drive/Motor_Drive.py
+++ Software_Layout/Motor_Drive/Motor_Drive.py
@@ -7,28 +7,52 @@ class Motor_Drive:
         self.bot_idle()
 
     def bot_idle(self):
+        while (self.motorL.duty_cycle > 0 or self.motorR.duty_cycle > 0):
+            self.motorL.decelerate()
+            self.motorR.decelerate()
+            print("we are decelerating")
         self.motorL.idle()
         self.motorR.idle()
         print("we are idle")
     
     def bot_forward(self):
+        # really need to keep track that the duty cycles for the motors are the same/return to being the same when they should
         self.motorL.forward()
         self.motorR.forward()
+        while (self.duty_cycle < MAX_DUTY_CYCLE or self.motorR.duty_cycle < MAX_DUTY_CYCLE):
+            self.motorL.accelerate()
+            self.motorR.accelerate()
+            print("we are accelerating")
         print("we are going forward")
 
+    # go idle before going backward
     def bot_backward(self):
         self.motorL.backward()
         self.motorR.backward()
+        while (self.duty_cycle < MAX_DUTY_CYCLE or self.motorR.duty_cycle < MAX_DUTY_CYCLE):
+            self.motorL.accelerate()
+            self.motorR.accelerate()
+            print("we are accelerating")
         print("we are going backward")
 
+    # go idle before making a turn
     def bot_clockwise(self):
         self.motorL.forward()
         self.motorR.backward()
+        while (self.duty_cycle < MAX_DUTY_CYCLE or self.motorR.duty_cycle < MAX_DUTY_CYCLE):
+            self.motorL.accelerate()
+            self.motorR.accelerate()
+            print("we are accelerating")
         print("we are turning clockwise")
 
+    # go idle before making a turn
     def bot_counter_clockwise(self):
         self.motorL.backward()
         self.motorR.forward()
+        while (self.duty_cycle < MAX_DUTY_CYCLE or self.motorR.duty_cycle < MAX_DUTY_CYCLE):
+            self.motorL.accelerate()
+            self.motorR.accelerate()
+            print("we are accelerating")
         print("we are turning counter-clockwise")
 
     def delete(self):
