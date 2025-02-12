2426c69a82d22781d569b8f95145a1621b7b0030
nehajoby <joby.n@northeastern.edu>
2025-02-11 20:33:35 -0500

    Minor updates made to Motor_Drive_Manager.py

--- Software_Layout/Motor_Drive/Motor_Drive_Manager.py
+++ Software_Layout/Motor_Drive/Motor_Drive_Manager.py
@@ -4,27 +4,34 @@
 
 from Motor_Drive import Motor_Drive
 from time import sleep
+
 class Motor_Drive_Manager:
     def __init__(self):
         self.Motor_Drive = Motor_Drive()
         self.Motor_Drive.bot_idle()
 
-
     def test(self):
         self.Motor_Drive.bot_forward()
         sleep(5)
+        
         self.Motor_Drive.bot_idle()
         sleep(2)
+        
         self.Motor_Drive.bot_backward()
         sleep(5)
+        
         self.Motor_Drive.bot_idle()
         sleep(2)
+        
         self.Motor_Drive.bot_clockwise()
         sleep(2)
+        
         self.Motor_Drive.bot_idle()
         sleep(2)
+        
         self.Motor_Drive.bot_counter_clockwise()
         sleep(2)
+        
         self.Motor_Drive.bot_idle()
         self.Motor_Drive.delete()
 
