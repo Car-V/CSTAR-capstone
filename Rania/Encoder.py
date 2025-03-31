f8c5671537d158cb8773a677eade19e28e3a73a6
Rania Alsha <70356704+raniaa03@users.noreply.github.com>
2025-03-31 09:54:16 -0400

    Update Encoder.py

--- Rania/Encoder.py
+++ Rania/Encoder.py
@@ -29,7 +29,8 @@ class Encoder:
         prev_b = self.last_b_state
         curr_a = GPIO.input(self.pin_a)
         curr_b = GPIO.input(self.pin_a)
-
+        print(prev_a, prev_b)
+        print(curr_a, curr_b)
         if (prev_a, prev_b) == (0, 0) and (curr_a, curr_b) == (1, 0):
             self.direction = 1  # Clockwise (00 -> 10)
         elif (prev_a, prev_b) == (1, 0) and (curr_a, curr_b) == (1, 1):
