82bd3c7d676e20deaa567bdce6c906f0cd0bac93
Rania Alsha <70356704+raniaa03@users.noreply.github.com>
2025-03-31 17:22:22 -0400

    Update Main.py

--- Rania/Main.py
+++ Rania/Main.py
@@ -32,8 +32,8 @@ GPIO.setup(en_b, GPIO.OUT)
 # PWM Setup
 q = GPIO.PWM(en_a, 100)  # Right motor PWM
 p = GPIO.PWM(en_b, 100)  # Left motor PWM
-p.start(15)
-q.start(15)
+p.start(25)
+q.start(25)
 
 # Stop motors initially
 GPIO.output(in1, GPIO.LOW)
